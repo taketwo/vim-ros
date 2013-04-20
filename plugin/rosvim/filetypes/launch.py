3074d14eefb5095ffbc5dcfda1012795e52fb77a
Sergey Alexandrov <alexandrov88@gmail.com>
2013-04-20 23:32:10 +0200

    Add partial_xml_parser utility.
    Also added test version of 'gf' mapping for launch files.

--- plugin/rosvim/filetypes/launch.py
+++ plugin/rosvim/filetypes/launch.py
@@ -1,6 +1,7 @@
 import vim
 import vimp
 import rosp
+import util.partial_xml_parser as pxp
 
 
 @vimp.function('ros#launch_complete')
@@ -29,6 +30,15 @@ def complete(findstart, base):
             return []
 
 
+@vimp.function('ros#launch_goto_file')
+def goto_file():
+    tag = pxp.get_inner_tag(vim.current.buffer, vimp.buf.cursor)
+    if 'file' in tag.attr:
+        import roslaunch.substitution_args
+        f = roslaunch.substitution_args.resolve_args(tag.attr['file'])
+        vimp.edit(f)
+
+
 def detect():
     return vimp.buf.extension == '.launch'
 
@@ -37,6 +47,7 @@ def init():
     vimp.opt['l:filetype'] = 'roslaunch.xml'
     vimp.opt['l:omnifunc'] = complete
     vimp.var['b:syntastic_checkers'] = ['rosvim']
+    vim.command('nnoremap <buffer> gf :call ros#launch_goto_file()<CR>')
 
 
 ###############################################################################
---
+++ plugin/util/partial_xml_parser.py
@@ -0,0 +1,83 @@
+from util.position import Position
+
+
+def _at(lines, pos):
+    try:
+        return lines[pos.line][pos.col]
+    except IndexError:
+        return None
+
+
+def _between(lines, s, e):
+    if s.line == e.line:
+        return lines[s.line][s.col:e.col + 1]
+    return ''.join([lines[s.line][s.col:]] +
+                   [lines[i] for i in range(s.line + 1, e.line - 1)] +
+                   [lines[e.line][:e.col + 1]])
+
+
+def _find(lines, pos, char):
+    if _at(lines, pos) == char:
+        return pos
+    i, j = pos.line, pos.col
+    try:
+        line = lines[i][j:]
+    except IndexError:
+        line = ''
+    while True:
+        try:
+            f = line.index(char)
+            return Position(i, len(lines[i]) - len(line) + f)
+        except ValueError:
+            i = i + 1
+            if i >= len(lines):
+                return None
+            line = lines[i]
+
+
+def _rfind(lines, pos, char):
+    if _at(lines, pos) == char:
+        return pos
+    i, j = pos.line, pos.col
+    try:
+        line = lines[i][:j]
+    except IndexError:
+        line = ''
+    while True:
+        try:
+            f = line.rindex(char)
+            return Position(i, f)
+        except ValueError:
+            i = i - 1
+            if i < 0:
+                return None
+            line = lines[i]
+
+
+class Tag(object):
+
+    def __init__(self, text):
+        import re
+        self.text = text
+        self.attr = dict()
+        self.partial = False if re.match(r'<.*>', text) else True
+        self.comment = True if re.match(r'<!--.*-->', text) else False
+        if self.comment:
+            return
+        g = re.match(r'<(\w+) *(.*?)[ /]?>?$', text)
+        if not g:
+            return
+        self.name = g.groups()[0]
+        self.attr = dict(re.findall(r'(\w+)="([^"]*)" ?', g.groups()[1]))
+
+
+def get_inner_tag(lines, pos):
+    so = _rfind(lines, pos, '<')
+    sc = _rfind(lines, pos, '>')
+    if sc is not None and sc != pos and sc > so:
+        return None
+    ec = _find(lines, pos, '>')
+    eo = _find(lines, pos, '<')
+    if ec is None or (eo is not None and eo != pos and eo < ec):
+        return _between(lines, so, pos)
+    return Tag(_between(lines, so, ec))
