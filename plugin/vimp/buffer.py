defb8269a92bea1b4eee7718c2da71822d70904b
Sergey Alexandrov <alexandrov88@gmail.com>
2013-04-20 21:29:12 +0200

    Add cursor property to Buffer.

--- plugin/vimp/buffer.py
+++ plugin/vimp/buffer.py
@@ -1,5 +1,6 @@
 import vim
 import os
+from util.position import Position
 
 
 class _Buffer(object):
@@ -38,3 +39,24 @@ class _Buffer(object):
     @property
     def number(self):
         return vim.current.buffer.number
+
+    @property
+    def cursor(self):
+        """
+        Get cursor position in the current buffer (Note: zero-based index).
+        """
+        c = vim.current.window.cursor
+        return Position(c[0] - 1, c[1])
+
+    @cursor.setter
+    def cursor(self, value):
+        """
+        Set cursor position in the current buffer (Note: zero-based index).
+        """
+        if isinstance(value, Position):
+            c = (value.line + 1, value.col)
+        elif isinstance(value, tuple) and len(value) == 2:
+            c = (value[0] + 1, value[1])
+        else:
+            raise TypeError('Unsupported type for cursor: %s' % type(value))
+        vim.current.window.cursor = c
