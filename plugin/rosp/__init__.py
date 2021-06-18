b676434dd925c85816ae79fb321878ae9c595f99
Sergey Alexandrov <alexandrov88@gmail.com>
2021-06-18 09:37:40 +0000

    Add rosp.list_messages()
    The function uses `rosmsg list` to get the list of all ROS messages in the system.

--- plugin/rosp/__init__.py
+++ plugin/rosp/__init__.py
@@ -5,7 +5,10 @@
 Utility functions and classes for ROS.
 """
 
+import subprocess
+
 from .package import Package
+from util import time_cache
 
 
 def list_nodelets():
@@ -13,6 +16,18 @@ def list_nodelets():
     List all nodelets in all packages.
     """
     nodelets = []
-    for p in rospkg.RosPack().get_depends_on('nodelet', implicit=False):
+    for p in rospkg.RosPack().get_depends_on("nodelet", implicit=False):
         nodelets += Package(p).list_nodelets()
     return nodelets
+
+
+@time_cache(10, maxsize=1)
+def list_messages():
+    """
+    List all messages in all packages.
+    """
+    return (
+        subprocess.check_output(["rosmsg", "list"], encoding="utf-8")
+        .strip()
+        .split("\n")
+    )
